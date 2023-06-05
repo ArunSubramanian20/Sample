21f53e1f1d3939f727b043c82818769ae187c0d6
barunprasanth <barunprasanth@LT1632-1>
2023-06-05 15:35:27 +0530

    Add project files.

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorldApp
+{
+
+
+    class Geeks
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
